0c8c6266b5713c91e0e5cff2c0409f2ef9295995
“Atharva <atharvacodes@gmail.com>
2024-06-19 18:45:10 +0530

    Day 1

---
+++ Basics.java
@@ -0,0 +1,10 @@
+public class Basics {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7 ;
+        int sum = a+b;
+
+        System.out.println("hello World " + sum);
+    }
+}
